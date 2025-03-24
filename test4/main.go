ead1c193be05fc44b3b09f7c01bb1f392bd3e537
Lenovo 15 <information@com1software.com>
2025-03-24 09:45:03 -0400

    update

--- test4/main.go
+++ test4/main.go
@@ -15,20 +15,31 @@ func main() {
 	a := app.New()
 
 	// Create the main window
-	w := a.NewWindow("Two Buttons")
+	w := a.NewWindow("Two Buttons with Memo")
+
+	// Create the text memo field with multiline and larger height
+	memo := widget.NewEntry()
+	memo.SetPlaceHolder("Enter your memo here...")
+	memo.MultiLine = true               // Enable multiline for larger text fields
+	memo.Resize(fyne.NewSize(400, 100)) // Adjust the height (4x the default)
+
+	// Create the "Say Hello" button
+	helloButton := widget.NewButton("Say Hello", func() {
+		// Display the value from the memo field in the dialog box
+		dialog.ShowInformation("Hello", "Hello, "+memo.Text, w)
+	})
 
 	// Create the "Exit" button
 	exitButton := widget.NewButton("Exit", func() {
 		os.Exit(0)
 	})
 
-	// Create the "Say Hello" button
-	helloButton := widget.NewButton("Say Hello", func() {
-		dialog.ShowInformation("Hello", "Hello, World!", w)
-	})
-
 	// Set the content of the window
-	w.SetContent(container.NewVBox(helloButton, exitButton))
+	w.SetContent(container.NewVBox(
+		memo,        // Add the memo field
+		helloButton, // Add the "Say Hello" button
+		exitButton,  // Add the "Exit" button
+	))
 
 	// Resize the window to make it larger
 	w.Resize(fyne.NewSize(400, 300))
