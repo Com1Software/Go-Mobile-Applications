733b102cc10affe9cacb614f48ce64ca8497bd78
Com1 Software, Inc. <information@com1software.com>
2025-03-24 09:36:03 -0400

    Create main.go

---
+++ test4/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	// Create the application
+	a := app.New()
+
+	// Create the main window
+	w := a.NewWindow("Two Buttons")
+
+	// Create the "Exit" button
+	exitButton := widget.NewButton("Exit", func() {
+		os.Exit(0)
+	})
+
+	// Create the "Say Hello" button
+	helloButton := widget.NewButton("Say Hello", func() {
+		dialog.ShowInformation("Hello", "Hello, World!", w)
+	})
+
+	// Set the content of the window
+	w.SetContent(container.NewVBox(helloButton, exitButton))
+
+	// Resize the window to make it larger
+	w.Resize(fyne.NewSize(400, 300))
+
+	// Show the window and run the application
+	w.ShowAndRun()
+}
