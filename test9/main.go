0ba7283d892e0aa52bc645c5c1bb769fc132e5ed
Lenovo 15 <information@com1software.com>
2025-05-25 09:58:27 -0400

    update

--- test9/main.go
+++ test9/main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"bufio"
-	"encoding/xml"
-	"fmt"
 	"image/color"
-	"log"
-	"net"
-	"net/http"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -17,14 +11,10 @@ import (
 )
 
 func main() {
-	xip := fmt.Sprintf("%s", GetOutboundIP())
-	port := "8080"
-	a := app.New()
-	w := a.NewWindow("Listening on " + xip + ":" + port)
 
-	//	button1a := NewCustomButton("", color.RGBA{0, 0, 255, 255}, func() {
-	//		println("Button clicked!")
-	//	})
+	a := app.New()
+	w := a.NewWindow("Testing")
+
 	button2a := NewCustomButton("", color.RGBA{0, 0, 255, 255}, func() {
 		println("Button clicked!")
 	})
@@ -81,106 +71,4 @@ func (b *CustomButton) Tapped(_ *fyne.PointEvent) {
 		b.bgRect.FillColor = color.RGBA{255, 255, 255, 255} // Change to Green
 	}
 	b.bgRect.Refresh() // Update UI
-}
-
-func GetOutboundIP() net.IP {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP
-}
-
-type message struct {
-	Controller string `xml:"controller"`
-	DateTime   string `xml:"date_time"`
-	Command    string `xml:"command"`
-	Value      string `xml:"value"`
-}
-
-func ReadURL(url string) string {
-	msg := &message{}
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	reader := bufio.NewReader(resp.Body)
-
-	for {
-		line, erra := reader.ReadBytes('\n')
-		if erra != nil {
-			log.Fatal(erra) // Ensure correct error logging
-		}
-		xml.Unmarshal(line, &msg)
-		break // Exit after the first read
-	}
-
-	// Construct the string with extracted values
-	return msg.Controller + " " + msg.DateTime + " " + msg.Command + " " + msg.Value
-}
-
-type Agent struct {
-	Notifier    chan []byte
-	newuser     chan chan []byte
-	closinguser chan chan []byte
-	user        map[chan []byte]bool
-}
-
-func SSE() (agent *Agent) {
-	agent = &Agent{
-		Notifier:    make(chan []byte, 1),
-		newuser:     make(chan chan []byte),
-		closinguser: make(chan chan []byte),
-		user:        make(map[chan []byte]bool),
-	}
-	go agent.listen()
-	return
-}
-
-func (agent *Agent) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	flusher, ok := rw.(http.Flusher)
-	if !ok {
-		http.Error(rw, "Error ", http.StatusInternalServerError)
-		return
-	}
-	rw.Header().Set("Content-Type", "text/event-stream")
-	rw.Header().Set("Cache-Control", "no-cache")
-	rw.Header().Set("Connection", "keep-alive")
-	rw.Header().Set("Access-Control-Allow-Origin", "*")
-	mChan := make(chan []byte)
-	agent.newuser <- mChan
-	defer func() {
-		agent.closinguser <- mChan
-	}()
-	notify := req.Context().Done()
-	go func() {
-		<-notify
-		agent.closinguser <- mChan
-	}()
-	for {
-		fmt.Fprintf(rw, "%s", <-mChan)
-		flusher.Flush()
-	}
-
-}
-
-func (agent *Agent) listen() {
-	for {
-		select {
-		case s := <-agent.newuser:
-			agent.user[s] = true
-		case s := <-agent.closinguser:
-			delete(agent.user, s)
-		case event := <-agent.Notifier:
-			for userMChan, _ := range agent.user {
-				userMChan <- event
-			}
-		}
-	}
-
 }
